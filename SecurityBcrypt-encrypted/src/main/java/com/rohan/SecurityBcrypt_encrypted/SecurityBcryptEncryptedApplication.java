3c67b0f892731a01e063aca55d444a4af66b19af
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-07-17 00:11:17 +0530

    Starting a Bycrypt-Encrypted

---
+++ SecurityBcrypt-encrypted/src/main/java/com/rohan/SecurityBcrypt_encrypted/SecurityBcryptEncryptedApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.SecurityBcrypt_encrypted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBcryptEncryptedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBcryptEncryptedApplication.class, args);
+	}
+
+}
---
+++ SecurityBcrypt-encrypted/src/test/java/com/rohan/SecurityBcrypt_encrypted/SecurityBcryptEncryptedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.SecurityBcrypt_encrypted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBcryptEncryptedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
