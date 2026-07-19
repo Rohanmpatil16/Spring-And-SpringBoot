90735593b4d65eed6f07cd9773b03c35e02527a7
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-07-19 18:45:44 +0530

    Start the program

---
+++ SpringSecurityJPABCrypt/src/main/java/com/example/SpringSecurityJPABCrypt/SpringSecurityJpabCryptApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityJPABCrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJpabCryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpabCryptApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityJPABCrypt/src/test/java/com/example/SpringSecurityJPABCrypt/SpringSecurityJpabCryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityJPABCrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJpabCryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
